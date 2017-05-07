61c7ed20ee841ded1daaf6b160bf3a38570e5a90
Nicholas Snape <n1ck5nap3@gmail.com>
2017-05-08 00:55:49 +0100

    Added Everything

---
+++ scripts/checkout.js
@@ -0,0 +1,7 @@
+function addToCart(g_id){
+    if (isNaN(g_id)){return}
+    
+    alert("Item has been added to the cart.");
+    
+    console.log(g_id);
+}
--- scripts/load-games.js
+++ scripts/load-games.js
@@ -1,3 +1,25 @@
-$.getJSON("assets/games.json", function(result){
-    console.log(result["test"]);
+const itemBackup = $(".item").clone();
+$(".item").remove();
+
+$.getJSON("assets/games.json", function(result, status){
+    if (status != "success"){return}
+    
+    $.each(result, function(g_id, g_details){
+        let newCard = itemBackup.clone();
+        newCard.children(".item-add").attr("onclick", "addToCart(" + g_id + ")");
+        newCard.children(".item-logo").attr("src", g_details["logo"]);
+        newCard.children("div").children(".item-name").html(g_details["name"]);
+        newCard.children("div").children(".item-price").html("£" + g_details["price"]);
+        
+        $.each(g_details["description"], function(i, txt){
+            newCard.children(".item-desc").append("<p>" + txt + "</p>")
+        })
+        //newCard.children(".item-desc").html(g_details["description"]);
+        newCard.children(".item-release").html(g_details["release"]);
+        newCard.children(".item-more").attr("href", "webpages/products/" + g_id + ".html");
+        
+        newCard.appendTo($("#content"));
+    });
+    
+    $("#content").append('<div style="clear: both; height: 1px; width: 100%"></div>');
 });
