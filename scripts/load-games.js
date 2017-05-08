1ff13bcba750e33132bc1a297e8ba5e49d29b9da
Nicholas Snape <n1ck5nap3@gmail.com>
2017-05-08 17:43:58 +0100

    alt tags

--- scripts/load-games.js
+++ scripts/load-games.js
@@ -8,13 +8,13 @@ $.getJSON("assets/games.json", function(result, status){
         let newCard = itemBackup.clone();
         newCard.children(".item-add").attr("onclick", "addToCart(" + g_id + ")");
         newCard.children(".item-logo").attr("src", g_details["logo"]);
+        newCard.children(".item-logo").attr("alt", g_details["name"]);
         newCard.children("div").children(".item-name").html(g_details["name"]);
         newCard.children("div").children(".item-price").html("£" + g_details["price"]);
         
         $.each(g_details["description"], function(i, txt){
             newCard.children(".item-desc").append("<p>" + txt + "</p>")
         })
-        //newCard.children(".item-desc").html(g_details["description"]);
         newCard.children(".item-release").html(g_details["release"]);
         newCard.children(".item-more").attr("href", "webpages/products/" + g_id + ".html");
         
